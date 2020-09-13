5f496f803e3d3dbcc642cecd11cca112564741b9
sahu74 <hara.sahu@gmail.com>
2020-09-13 12:57:02 +0530

    span comment corrected

--- STD_SPAN/std_span.cpp
+++ STD_SPAN/std_span.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 // std::span
-/! A span is a view (i.e. non-owning) of a container providing bounds-checked access to a contiguous group of elements. 
+//! A span is a view (i.e. non-owning) of a container providing bounds-checked access to a contiguous group of elements. 
 //! Since views do not own their elements they are cheap to construct and copy -- 
 //! a simplified way to think about views is they are holding references to their data. Spans can be dynamically-sized or fixed-sized.
 
